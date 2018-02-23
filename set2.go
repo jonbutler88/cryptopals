ba2832cac25914c019229362cbae97c3433c582d
Jon <jonbutler88@gmail.com>
2018-02-23 12:03:46 +0000

    Finished 9 & 10

--- set2.go
+++ set2.go
@@ -1,1 +1,34 @@
 package cryptopals
+
+import (
+	"crypto/aes"
+)
+
+func pkcs7Pad(input []byte, blockSize int) []byte {
+	result := input
+	paddingSize := blockSize % len(input)
+
+	for i := 0; i < paddingSize; i++ {
+		result = append(result, byte(paddingSize))
+	}
+
+	return result
+}
+
+func cbcDecrypt(ciphertext []byte, key []byte, iv []byte) []byte {
+	plaintext := make([]byte, 0, len(ciphertext))
+
+	block, _ := aes.NewCipher(key)
+	currentXor := iv
+	currentBlock := make([]byte, block.BlockSize())
+
+	for i := 0; i < len(ciphertext); i += block.BlockSize() {
+		block.Decrypt(currentBlock, ciphertext[i:i+block.BlockSize()])
+		plaintext = append(plaintext, fixedXor(currentBlock, currentXor)...)
+
+		// Update the XOR
+		currentXor = ciphertext[i : i+block.BlockSize()]
+	}
+
+	return plaintext
+}
--- set2_test.go
+++ set2_test.go
@@ -1,7 +1,29 @@
 package cryptopals
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func Test9(t *testing.T) {
+	input := []byte("YELLOW SUBMARINE")
+	paddedInput := pkcs7Pad(input, 20)
+	if bytes.Compare(paddedInput, []byte("YELLOW SUBMARINE\x04\x04\x04\x04")) != 0 {
+		t.Errorf("Test failed, got: %s", string(paddedInput))
+	}
+}
 
+func Test10(t *testing.T) {
+	ciphertext, err := readBase64File("data/10.txt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	key := []byte("YELLOW SUBMARINE")
+	iv := make([]byte, 16)
+	plaintext := cbcDecrypt(ciphertext, key, iv)
+
+	if bytes.Compare(plaintext[0:33], []byte("I'm back and I'm ringin' the bell")) != 0 {
+		t.Errorf("Test failed, got: %s", string(plaintext[0:33]))
+	}
 }
